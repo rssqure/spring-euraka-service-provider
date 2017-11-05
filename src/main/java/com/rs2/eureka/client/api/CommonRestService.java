0aac8cf3bd18597bdb52bcf8100eca04a0b20795
Rajesh Kumar Somarppa <rsomarppa@Rajeshs-MacBook-Pro.local>
2017-11-05 10:31:32 -0600

    added cloud config client

--- src/main/java/com/rs2/eureka/client/api/CommonRestService.java
+++ src/main/java/com/rs2/eureka/client/api/CommonRestService.java
@@ -2,16 +2,25 @@ package com.rs2.eureka.client.api;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class CommonRestService {
 	Logger logger = LogManager.getLogger(CommonRestService.class.getName());
+	
+	@Value("${client.message}")
+	private String clientMessage;
+
+	@Value("${env.name}")
+	private String env;
 	
     @RequestMapping("/provider/service")
     public String service() {
     		logger.trace("In common service");
-        return "Hello from Common Service!";
+        return clientMessage + " on " + env;
     }
 }
